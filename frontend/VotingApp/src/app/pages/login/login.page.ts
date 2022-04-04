61cab97610c1dc5f99d19ca5c92e32cbbb7e5101
Connie Compos <beebaabado@gmail.com>
2022-04-04 17:27:34 -0600

    async calls and updating page issues fixed with statistics.  SQLite concurrent access issues (SQL_BUSY)  partially resolve..with adding finally to subscribes

--- frontend/VotingApp/src/app/pages/login/login.page.ts
+++ frontend/VotingApp/src/app/pages/login/login.page.ts
@@ -47,7 +47,8 @@ export class LoginPage {
          return;
       }
 
-      this.voterService.login(this.loginName).subscribe(result => {
+      this.voterService.login(this.loginName).subscribe(
+        (result) => {
           console.log("Voter info: ", result);
           this.voter = result;
           console.log(this.voter);
@@ -55,7 +56,13 @@ export class LoginPage {
           this.loggedIn = true;
           this.hideVoting = false;
           return result;
-        });
+        },
+        (err) => { console.log("Login Voter error: ", err)},
+        () => {
+                console.log("Login Voter: done");
+                this.disableVoterQuestions();
+              }
+       );
    }
 
     logout(){
@@ -67,38 +74,83 @@ export class LoginPage {
       this.hideQuestions = true;
       this.hideVoting = true;
       this.hideYesNo = true;
+      this.enableQuestions();
+      this.selectedQuestion = 0;
       this.displayMessage("Thanks for voting!");
     }
 
     getVoter(id:any){
-      this.voterService.getVoter(id).subscribe(result => (
-          console.log("Voter with list of unansered questions: ", result)
-      ));
+      this.voterService.getVoter(id).subscribe(
+        (result) => { console.log("Voter with list of unansered questions: ", result) },
+        (err) => { console.log("Get Voter error: ", err)},
+          () => {console.log("Get Voter: done")}
+      );
     }
     
     saveVote(vote:any){
       this.voter.questions.push(this.selectedQuestionId);
-      this.voterService.saveVote(vote, this.selectedQuestion).subscribe(result => {
+      this.voterService.saveVote(vote, this.selectedQuestion).subscribe(
+        (result) => {
           this.displayMessage("Your vote for question " + result['description'] 
-                                  + ": " + this.vote_values[vote] + " has been recoreded.");
-      }).unsubscribe();
-
-      this.voterService.saveVoter(this.voter.id, this.selectedQuestionId).subscribe(result => {
-        console.log("Voter question list updated: ", result);
-      });
+                                  + ": " + this.vote_values[vote] + " has been recorded.")},
+          (err) => { 
+              console.log("Save Vote error: ", err)
+              this.displayMessage("Oops! There was a problem saving your vote.  Please try again.")},
+          () => {
+            console.log("Save Vote: done");
+            this.disableQuestions();}
+      );
+      
+      // TODO On Database side need to check for trying to save duplicate question
+      this.saveVoter(this.voter.id, this.selectedQuestionId);
       this.hideYesNo = true;
-      this.displayMessage("Your vote has been recorded:  " + vote);
       this.disableQuestions(); 
     }
     
     disableQuestions() {
         let options = document.getElementById("display-questions").children;
-        console.log(this.selectedQuestion);
+        console.log("disable selected question: ", this.selectedQuestion);
         options.item(this.selectedQuestion).setAttribute("disabled", "disabled");
+
+        if (this.voter.questions.length >= this.questions.length) {
+          this.displayMessage("All questions have been answered.  Thank you for voting.");
+          this.hideVoting = true;
+        }
     }
 
-    saveVoter(voter_id, question_id){
-      this.voterService.saveVoter(this.voter.id, question_id);
+    disableVoterQuestions() {
+      let options = document.getElementById("display-questions").children;
+      let sel_id = 0
+      this.voter.questions.forEach (question_id => {
+        console.log("disable question with id: ",question_id);
+        for (let i = 0; i < options.length; i++) {
+          if (options[i].getAttribute('value') == question_id) {
+            options[i].setAttribute("disabled", "disabled");
+          }
+      }});
+      
+      if (this.voter.questions.length >= this.questions.length) {
+        this.displayMessage("All questions have been answered.  Thank you for voting.");
+        this.hideVoting = true;
+      }  
+    }
+    
+    enableQuestions() {
+      let options = document.getElementById("display-questions").children;
+      console.log("disable selected question: ", this.selectedQuestion);
+      // skip first item...placeholder
+      for (let i = 1; i < options.length; i++) { 
+        options.item(i).setAttribute("disabled", "");
+      }    
+    }
+
+    saveVoter(voter_id, question_id){    
+      this.voterService.saveVoter(voter_id, question_id).subscribe(
+        (result) => {
+          console.log("Save Voter results: ", result);},
+        (err) => { console.log("Save Voter error: ", err)},
+        () => {console.log("Save Voter: done")}
+        );
     }
     
     selectQuestionChange(event: any){
@@ -118,18 +170,20 @@ export class LoginPage {
     
     getQuestions(){
       this.questions = []
-      this.voterService.getQuestions().subscribe(result => {
-          result.forEach (question => {
-          var newQuestion = new Question();
-        // newQuestion = question;
-          newQuestion.id = question.id;
-          newQuestion.counts_no = question.counts_no;
-          newQuestion.counts_yes = question.counts_yes;
-          newQuestion.description = question.description;
-          this.questions.push(newQuestion);
-
-        })
-      });
+      this.voterService.getQuestions().subscribe(
+        (result) => {
+            result.forEach (question => {
+            var newQuestion = new Question();
+          // newQuestion = question;
+            newQuestion.id = question.id;
+            newQuestion.counts_no = question.counts_no;
+            newQuestion.counts_yes = question.counts_yes;
+            newQuestion.description = question.description;
+            this.questions.push(newQuestion);})},
+        (err) => { console.log("Get Questions error: ", err)},
+        () => {console.log("Get Questions: done")}
+      );   
+    
       console.log("Questions list: ", this.questions);
     }
     
--- frontend/VotingApp/src/app/pages/vote/vote.page.ts
+++ frontend/VotingApp/src/app/pages/vote/vote.page.ts
@@ -24,16 +24,23 @@ export class VotePage {
 
     getQuestionStats(){
       this.stats = new Stats();
-      this.voterService.getStats(this.selectedQuestion).subscribe(result => {
-          console.log("Stat info for question: ", result);
+      this.voterService.getStats(this.selectedQuestion).subscribe(
+        (result) => {
           this.stats.counts_no = result.counts_no;
           this.stats.counts_yes = result.counts_yes;
           this.stats.percent_no = result.percent_no;
           this.stats.percent_yes = result.percent_yes;
+          this.stats.total_votes = result.total_votes;
           this.stats.id = result.id;
-      })
-      //console.log("Stats: ",this.stats);
-      this.displayStats();
+          console.log("Stat info for question: ", this.stats);
+      },
+       (err) => { console.log("Get question stats error: ", err)},
+       () => { 
+          console.log("Get question stats: done")
+          //console.log("Stats: ",this.stats);
+          this.displayStats();}
+      );
+      
     }
     
     getQuestions(){
@@ -68,12 +75,12 @@ export class VotePage {
       var statsList = document.getElementById("display-stats");
       console.log("display stats: ", Object.keys(this.stats));
       statsList.innerHTML = '';
+      console.log("UGH: ", this.stats.counts_no);
       Object.keys(this.stats).forEach( (key) => {
         console.log("key: ", key)
          if (key != "id") {
           var  li = document.createElement("li");
-          console.log(this.stats[key])
-          li.innerHTML = key + ": " + this.stats[key];
+          li.innerHTML = key + ": " + this.stats[key]; 
           statsList.appendChild(li);
          }
       })
